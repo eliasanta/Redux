ba691546ce65534eaca7ac636d362e7398065d92
elia <eliasanta93@gmail.com>
2023-10-14 15:23:40 +0200

    some test

---
+++ redux-store.js
@@ -0,0 +1,70 @@
+//Actions creations
+//Person who is submitting the form
+const newBooking=(name,amount)=>{
+    return{
+        type:"NEW_BOOKING",
+        payload:{
+            name,
+            amount,
+        }
+    }
+}
+
+const cancelBooking=(name,refundAmount)=>{
+    return{
+        type:"CANCEL_BOOKING",
+        payload:{
+            name,
+            refundAmount,
+        }
+    }
+}
+
+//reducers
+const reservationHistory=(oldReservationList=[],action) =>{
+    if(action.type==="NEW_BOOKING"){
+
+        return [...oldReservationList,action.payload];
+    }else if(action.type==="CANCEL_BOOKING"){
+
+        return oldReservationList.filter((record)=>{
+            return record.name !== action.payload.name
+        });
+    }
+    return oldReservationList
+}
+
+const cancellationHistory=(oldCancellationList=[],action) =>{
+    if(action.type==="CANCEL_BOOKING"){
+        return [...oldCancellationList, action.payload]
+    };
+    return oldCancellationList
+}
+
+const accounting=(totalMoney = 100 ,action) =>{
+    if(action.type === "NEW_BOOKING"){
+        return totalMoney + action.payload.amount;
+    }
+    else if(action.type==="CANCEL_BOOKING"){
+        return totalMoney - action.payload.refundAmount;
+    };
+    return totalMoney
+}
+
+//redux Store
+
+const{createStore,combineReducers}=Redux;
+
+const railwayCentralStore = combineReducers({
+    accounting:accounting,
+    reservationHistory:reservationHistory,
+    cancellationHistory:cancellationHistory,
+})
+
+const store =createStore(railwayCentralStore)
+const action =newBooking("ELia", 25)
+store.dispatch(action)//quello che fa eseguire l'azione
+store.dispatch(newBooking("Gianni", 25))
+store.dispatch(cancelBooking("ELia", 10))
+
+console.log(store.getState())
